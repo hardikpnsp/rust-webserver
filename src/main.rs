e94c956fdd14373ebe50b5d56ac29296b71ad0fd
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-17 10:07:38 +0530

    ENH: rendering simple html page

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,11 +17,10 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
 
     stream.read(&mut buffer).unwrap();
-
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-
+    
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+    println!("{}", response);     
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
