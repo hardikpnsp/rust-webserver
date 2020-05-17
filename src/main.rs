edc5ad638ef3dc1bd5505caa6c0fd25d789c26cf
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-17 10:18:26 +0530

    ENH: handling of unexpected requests with 404

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,24 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
     
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-    println!("{}", response);     
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if buffer.starts_with(get) {
+
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        println!("{}", response);     
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
