f88bf548c7185eb5e50b541bbae7d524f14e1d79
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-20 21:46:00 +0530

    ENH: added channel for communication between threads

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>, 
 }
+
+struct Job;
 
 impl ThreadPool {
     /// Create a new ThreadPool
@@ -14,14 +20,18 @@ impl ThreadPool {
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
+       
+        let (sender, receiver) = mpsc::channel();
         
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
         
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f:F)
@@ -37,8 +47,10 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
         Worker { id, thread }
     }
 }
