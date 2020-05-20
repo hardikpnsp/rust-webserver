c502995127bce7815dd36c9c569ba75f717c8bfa
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-20 08:39:53 +0530

    ENH: added sleep route for checking multi-threaded effectiveness

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,12 +18,17 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-
+    println!("{}",String::from_utf8_lossy(&buffer[..]));
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
-    } else {
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    }
+    else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
