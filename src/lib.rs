ab7ea4eaaaaa206677c980ee856b476a0abb235c
Hardik Prajapati <hardikpnsp@gmail.com>
2020-05-21 08:29:22 +0530

    ENH: impl Drop trait on ThreadPool and wrapped thread in Option enum

--- src/lib.rs
+++ src/lib.rs
@@ -44,9 +44,20 @@ impl ThreadPool {
     }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -58,6 +69,6 @@ impl Worker {
 
             job();
         });
-        Worker { id, thread }
+        Worker { id, thread: Some(thread) }
     }
 }
